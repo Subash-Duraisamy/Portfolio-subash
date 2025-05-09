5b590c2bcc18c85b730be3f0f8687fc43f3f6a3e
Subash Duraisamy <subash111425@email.com>
2025-05-09 12:12:00 +0530

    audio removed

--- src/Components/Pages/Home.js
+++ src/Components/Pages/Home.js
@@ -1,62 +1,21 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
 import './Home.css';
-import { FaPlay, FaPause } from 'react-icons/fa';
+
 
 function Home() {
-  const audioRef = useRef(null);
-  const [isPlaying, setIsPlaying] = useState(false);
 
-  // useEffect(() => {
-  //   const handleInteraction = () => {
-  //     if (audioRef.current) {
-  //       audioRef.current.play().catch(err => {
-  //         console.warn("Still blocked:", err);
-  //       });
-  //     }
 
-  //     // Remove event listeners once the audio starts playing
-  //     window.removeEventListener('click', handleInteraction);
-  //     window.removeEventListener('scroll', handleInteraction);
-  //   };
 
-  //   // Attach event listeners to detect user interaction (click or scroll)
-  //   window.addEventListener('click', handleInteraction);
-  //   window.addEventListener('scroll', handleInteraction);
-
-  //   return () => {
-  //     // Clean up event listeners when the component unmounts
-  //     window.removeEventListener('click', handleInteraction);
-  //     window.removeEventListener('scroll', handleInteraction);
-
-  //     // Pause the audio if the component is unmounted
-  //     if (audioRef.current) {
-  //       audioRef.current.pause();
-  //     }
-  //   };
-  // }, []);
-
-  // Function to handle play/pause
-  const handlePlayPause = () => {
-    if (audioRef.current) {
-      if (isPlaying) {
-        audioRef.current.pause();
-      } else {
-        audioRef.current.play();
-      }
-      setIsPlaying(!isPlaying);
-    }
-  };
+  
 
   return (
     <div className="home-container">
+      {/* Optional: If you want audio later, add this <audio> tag back */}
       {/* <audio ref={audioRef} src="/door_creaky.mp3" loop preload="auto" /> */}
-      
+
       <div className="home-left">
         <h1 className="name-title">Subash D</h1>
-        <span className="play-icon" onClick={handlePlayPause}>
-          {isPlaying ? <FaPause /> : <FaPlay />}
-        </span>
-
+        
         <p className="pendulum-text">Junior Developer</p>
       </div>
 
