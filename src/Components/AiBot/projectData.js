7abcdb82e329df5ce038b68dcb4db51174d229c2
SUBASHOFFI <redmisubashias@gmail.com>
2025-12-04 22:04:07 +0530

    updated bot

--- src/Components/AiBot/projectData.js
+++ src/Components/AiBot/projectData.js
@@ -1,5 +1,11 @@
 // src/projectData.js
 export const projects = {
+  "Author": {
+    title: "Subash",
+    description:
+      "Hey, this is Subash!I'm a final-year engineering student who’s deeply passionate about tech, AI, and building real-world projects.I started my journey with Java and C++, but now I’m fully focused on Python, MERN stack, AI/ML, and full-stack development.I love creating apps that solve real problems—from streak trackers to full Career Guidance platforms with AI.I’m also exploring data science, cloud, FastAPI, React, Firebase, and deep learning, always pushing myself to learn something new every day.I’m the type of person who picks up an idea, works consistently, and turns it into something meaningful.Focused, curious, and improving day by day — that’s me, Subash.",
+    tech: "I am learning AI/ML, Python, MERN Stack, Full-Stack Development, Data Science, Cloud Computing, FastAPI, React, Firebase, Deep Learning" ,
+  },
   "bus-tracker": {
     title: "College Bus Tracker",
     description:
